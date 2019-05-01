568acd2f45c056a7fbd8bc0776b6cd1c1e4f17c2
kellylyke <42480491+kellylyke@users.noreply.github.com>
2019-05-01 15:05:12 -0500

    took dependencies on shared datas service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SharedDataService } from '../shared-data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+    constructor(private sharedDataSvc: SharedDataService) {}
 }
