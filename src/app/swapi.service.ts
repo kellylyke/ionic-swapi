613dbf60c3a38e6db69a19cb15e21ee2262a544b
kellylyke <42480491+kellylyke@users.noreply.github.com>
2019-05-01 16:16:06 -0500

    rewrote with expand and recursion

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { merge } from 'rxjs';
+import { merge, empty } from 'rxjs';
+import {expand} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,13 @@ export class SwapiService {
   constructor(private httpSvc: HttpClient) { }
 
   getPlanets() {
-    const p1 = this.httpSvc.get("https://swapi.co/api/planets/");
-    const p2 = this.httpSvc.get("https://swapi.co/api/planets?page=2");
+    // const p1 = this.httpSvc.get("https://swapi.co/api/planets/");
+    // const p2 = this.httpSvc.get("https://swapi.co/api/planets?page=2");
 
-    return merge(p1, p2);
+    // return merge(p1, p2);
+
+    return this.httpSvc.get("https://swapi.co/api/planets/").pipe(
+      expand(data => (<any>data).next ? this.httpSvc.get((<any>data).next) : empty())
+    );
   }
 }
