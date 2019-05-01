c5df66efca25bbe80a1b2a18d68f4fcbbb0ac7bc
kellylyke <42480491+kellylyke@users.noreply.github.com>
2019-05-01 16:04:03 -0500

    more planets

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -32,7 +32,7 @@ export class ListPage implements OnInit {
     this.swapiSvc.getPlanets().subscribe(
       data => {
           console.log(data);
-          this.items = (<any>data).results.map(x => x.name);
+          this.items = [...this.items, ...(<any>data).results.map(x => x.name)].sort();
       }, error => console.log(error)
     );
   }
